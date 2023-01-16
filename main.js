bdb74302cc798f19c55496bc5db301c5ebde6f5e
Igor Tude <igortude@hotmail.com>
2023-01-15 21:28:45 -0300

    Added files - commit inicial

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ $('header button').click(function (){
 $('#btn-cancel').click(function (){
     $('form').slideUp();
 })
+
+$('form').on('submit', function(e){
+    e.preventDefault();
+
+    const novaImg = $('#adicionarImg').val();
+    const novoItem = $('<li style="display:none"></li>');
+    $(`<img src="${novaImg}" />`).appendTo(novoItem);
+    $(`<div class="overlay-bar">
+        <a href="${novaImg}" target="_blank" title="Ver imagem em tamanho real">
+            Ver Imagem em tamanho real
+        </a>
+    </div>`).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
+    $(novoItem).fadeIn(2000);
+    $('#adicionarImg').val('');
+
+})
